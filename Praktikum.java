f61a7b034a0827bb30a017006355662ebcfb2a9f
Bogdan Diakov <bogdandiakov@gmail.com>
2024-12-21 11:43:51 +0500

    from linux IDEA third my conflict

--- Praktikum.java
+++ Praktikum.java
@@ -15,7 +15,7 @@ public class Praktikum {
     }
 	System.out.println("first_conflict");
 	System.out.println("second_my_conflict");
-	System.out.println("Third_conflict");
+	System.out.println("Third_my_conflict");
   }
 
 }
